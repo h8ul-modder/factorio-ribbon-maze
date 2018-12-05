16c556204e3f398b6617a53dcb09ebfb018c0a5c
H8UL <h8ul.mods@gmail.com>
2018-12-05 14:26:16 +0000

    mangrove migration for existing saves, refs #16

---
+++ migrations/2018-12-05_RibbonMaze_0.6.7.lua
@@ -0,0 +1,12 @@
+
+for _,surface in pairs(game.surfaces) do
+
+    for _, bruguiera in pairs(surface.find_entities_filtered{name="mangrove-bruguiera"}) do
+        if bruguiera.valid then
+            if surface.get_tile(bruguiera.position.x, bruguiera.position.y).name ~= "water" then
+                bruguiera.destroy()
+            end
+        end
+    end
+end
+
