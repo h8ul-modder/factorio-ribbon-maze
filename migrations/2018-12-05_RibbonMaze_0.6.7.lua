a07a395bba291980cac2a08f294fb839ba02a258
H8UL <h8ul.mods@gmail.com>
2018-12-05 14:29:45 +0000

    minor, comment

--- migrations/2018-12-05_RibbonMaze_0.6.7.lua
+++ migrations/2018-12-05_RibbonMaze_0.6.7.lua
@@ -1,4 +1,6 @@
 
+-- https://github.com/h8ul-modder/factorio-ribbon-maze/issues/16
+-- Removes mangroves that were not correctly colliding with landfill
 for _,surface in pairs(game.surfaces) do
 
     for _, bruguiera in pairs(surface.find_entities_filtered{name="mangrove-bruguiera"}) do
